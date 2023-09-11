382a22563390bfbbf7b70805048ccb67a37b36e5
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-09-11 18:20:47 +0200

    fix(path): wrong path to type def

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import { COMPONENTS, isComponentName, type ComponentName} from "./types/component.type";
-import { DAMAGE_TYPES, isDamageType, type DamageType } from "./types/damage-type.type";
-import { GROUPS, isGroupName, type GroupName } from "./types/group.type";
-import { SCHOOLS, isSchoolName, type SchoolName } from "./types/school.type";
-import { SOURCES, isSourceName, type SourceName } from "./types/source.type";
-import { SPELL_LEVELS, isSpellLevel, type SpellLevel } from "./types/spell-level.type";
+import { COMPONENTS, isComponentName} from "./types/component.type";
+import type { ComponentName } from "./types/component.type";
+import { DAMAGE_TYPES, isDamageType } from "./types/damage-type.type";
+import type { DamageType } from "./types/damage-type.type";
+import { GROUPS, isGroupName } from "./types/group.type";
+import type { GroupName } from "./types/group.type";
+import { SCHOOLS, isSchoolName } from "./types/school.type";
+import type { SchoolName } from "./types/school.type";
+import { SOURCES, isSourceName } from "./types/source.type";
+import type { SourceName } from "./types/source.type";
+import { SPELL_LEVELS, isSpellLevel } from "./types/spell-level.type";
+import type { SpellLevel } from "./types/spell-level.type";
 
+export type { ComponentName, DamageType, GroupName, SchoolName, SourceName, SpellLevel };
 export { 
-    COMPONENTS, isComponentName, ComponentName,
-    DAMAGE_TYPES, isDamageType, DamageType,
-    GROUPS, isGroupName, GroupName,
-    SCHOOLS, isSchoolName, SchoolName,
-    SOURCES, isSourceName, SourceName,
-    SPELL_LEVELS, isSpellLevel, SpellLevel
+    COMPONENTS, isComponentName, 
+    DAMAGE_TYPES, isDamageType, 
+    GROUPS, isGroupName, 
+    SCHOOLS, isSchoolName, 
+    SOURCES, isSourceName, 
+    SPELL_LEVELS, isSpellLevel,
 };
