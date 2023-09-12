c2e3222c638b176311b5ad126423287a652704af
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-09-12 16:05:46 +0200

    feat: content is all lower case

--- src/types/component.type.ts
+++ src/types/component.type.ts
@@ -1,4 +1,4 @@
-export const COMPONENTS = ["V", "S", "M"] as const;
+export const COMPONENTS = ["v", "s", "m"] as const;
 export type ComponentName = (typeof COMPONENTS)[number];
 export const isComponentName = (value: unknown): value is ComponentName => {
   return COMPONENTS.includes(value as ComponentName);
--- src/types/school.type.ts
+++ src/types/school.type.ts
@@ -1,12 +1,12 @@
 export const SCHOOLS = [
-  "Abjuration",
-  "Conjuration",
-  "Divination",
-  "Enchantment",
-  "Evocation",
-  "Illusion",
-  "Necromancy",
-  "Transmutation",
+  "abjuration",
+  "conjuration",
+  "divination",
+  "enchantment",
+  "evocation",
+  "illusion",
+  "necromancy",
+  "transmutation",
 ] as const;
 export type SchoolName = (typeof SCHOOLS)[number];
 export const isSchoolName = (value: unknown): value is SchoolName => {
